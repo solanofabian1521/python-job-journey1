78f4871c75615abd473366176530c8355288d012
Fabian Solano <solanofabian889@gmail.com>
2026-09-09 21:04:05 -0600

    feat: add candidate list

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 def registrar_candidato(nombre, edad):
 
     if edad >= 18:
+        candidatos.append(nombre)
         return "Candidato " + nombre + " registrado correctamente. "
     else: 
+           
         return nombre + " no cumple con la edad miníma. " 
+
+candidatos = []
 
 nombre = input("Ingrese el nombre: ")
 edad = int(input("Ingrese la edad: "))
 
 resultado = registrar_candidato(nombre,edad)
 
-print(resultado)
+print(resultado) 
+
+print("\n===== CANDIDATOS REGISTRADOS =====")
+
+for candidato in candidatos: 
+    print(candidato)
+
+    
